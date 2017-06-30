1faa66a9871296d95d54c1f3c8a8134b1a6503f8
jaisudha <jaisusasi@gmail.com>
2017-06-30 10:07:18 -0700

    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',//use template instead of templateurl
+  //styleUrls: ['./app.component.css']
+
+})
+export class AppComponent {
+name="nmy first angular 4 app";
+}
